6d108b6df61000b77ae561749d59ffc358fd9948
hringbauer <hringbauer@uchicago.edu>
2019-02-28 20:30:10 -0600

    Atom Sync

--- Python3/hmm_inference.py
+++ Python3/hmm_inference.py
@@ -22,17 +22,17 @@ class HMM_Analyze(object):
 
     def load_data(self, folder="./Simulated/Example0/"):
         """Loads the Data"""
-        self.ref_stats = np.loadtxt(folder + "refs.csv", dtype="int", delimiter=",")
-        self.ob_stat = np.loadtxt(folder + "hap.csv", dtype="int", delimiter=",")
+        self.ref_stats = np.loadtxt(
+            folder + "refs.csv", dtype="int", delimiter=",")
+        self.ob_stat = np.loadtxt(
+            folder + "hap.csv", dtype="int", delimiter=",")
 
         print(f"Successfully loaded from: {folder}")
 
 
-
 ###############################
 ###############################
-### Do some Testing
-
+# Do some Testing
 hmm = HMM_Analyze()
 hmm.load_data(folder="./Simulated/Example0/")
 print(np.shape(hmm.ref_stats))
