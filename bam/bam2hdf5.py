74c2592ffcd0ca3f36852d11c9a3069abf4b0ce2
hyl317 <yh362@cornell.edu>
2021-09-30 10:11:46 +0200

    add grid search to bam reader as well

--- bam/bam2hdf5.py
+++ bam/bam2hdf5.py
@@ -133,10 +133,21 @@ if __name__ == '__main__':
         sys.exit()
 
     sys.path.insert(0, "/mnt/archgen/users/yilei/tools/hapROH/package")
+
+    from hapsburg.PackagesSupport.hapsburg_run import hapCon_chrom
     from hapsburg.PackagesSupport.hapsburg_run import hapCon_chrom_BFGS
     from hapsburg.PackagesSupport.hapsburg_run import hapCon_chrom_2d
 
-    mle1, low95, up95 = hapCon_chrom_BFGS(iid, 'X', save=False, save_fp=False, n_ref=2504, diploid_ref=True, 
+    _, mle, low95, up95 = hapCon_chrom(iid, 'X', save=False, save_fp=False, n_ref=2504, diploid_ref=True, 
+        exclude_pops=[], conPop=[], e_model="readcount_contam", p_model="SardHDF5", 
+        readcounts=True, random_allele=False, post_model="Standard", path_targets = path2hdf5, 
+        folder_out='/mnt/archgen/users/yilei/Data/iberian_BAM/hapCon/',
+        h5_path1000g='/mnt/archgen/users/yilei/Data/1000G/1000g1240khdf5/all1240/chr',
+        meta_path_ref='/mnt/archgen/users/yilei/Data/1000G/1000g1240khdf5/all1240/meta_df_all.csv', 
+        prefix_out=iid, c=np.arange(0, 0.2, 0.005), roh_in=1, roh_out=0, roh_jump=300, e_rate=err, e_rate_ref=0.0,
+        max_gap=0, cutoff_post = 0.999, roh_min_l = 0.01, logfile=False)
+
+    mle_bfgs, low95_bfgs, up95_bfgs = hapCon_chrom_BFGS(iid, 'X', save=False, save_fp=False, n_ref=2504, diploid_ref=True, 
         exclude_pops=[], conPop=[], e_model="readcount_contam", p_model="SardHDF5", 
         readcounts=True, random_allele=False, post_model="Standard", path_targets = path2hdf5, 
         folder_out='/mnt/archgen/users/yilei/Data/iberian_BAM/hapCon/',
@@ -157,9 +168,10 @@ if __name__ == '__main__':
 
     with open(f'{iid}.hapcon.trim5.txt', 'w') as out:
         out.write(f'Number of target sites covered by at least one read: {numSitesCovered}\n')
-        out.write(f'Method1: BFGS on contamination rate\n')
+        out.write(f'Method1: Fixing genotyping error rate\n')
         out.write(f'\tEstimated genotyping error via flanking region: {round(err, 6)}\n')
-        out.write(f'\tMLE for contamination: {round(mle1[0], 6)} ({round(low95[0], 6)} - {round(up95[0], 6)})\n')
+        out.write(f'\tMLE for contamination using grid search: {round(mle, 6)} ({round(low95, 6)} - {round(up95, 6)})\n')
+        out.write(f'\tMLE for contamination using BFGS: {round(mle_bfgs[0], 6)} ({round(low95_bfgs[0], 6)} - {round(up95_bfgs[0], 6)})\n')
         out.write("\n")
         out.write(f'Method2: BFGS on contamination and genotyping error jointly\n')
         out.write(f'\tcontamination: {round(mle2[0], 6)}({round(mle2[0] - 1.96*se[0], 6)} - {round(mle2[0] + 1.96*se[0], 6)})\n')
